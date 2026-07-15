3ed56e10f909e6d22f6929cf94e9996c4ef35eeb
Andreas Terhoven <andreas.terhoven@live.de>
2026-07-15 05:34:52 +0200

    we have a game window now. with working wuit button!

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import pygame
+from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
+    #initializing pygame
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        #processing the pygame event queue
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
     
 
 
