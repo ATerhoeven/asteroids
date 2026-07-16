82578c1fb964400aaffd2278e489507a179ba795
Andreas Terhoven <andreas.terhoven@live.de>
2026-07-16 21:08:57 +0200

    Sprites Drawing Player and rotating

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,7 +10,16 @@ def main():
     #initializing pygame
     pygame.init()
 
+    #new clock object
+    clock = pygame.time.Clock()
+
+    #Delta time initialized as float
+    dt = 0.0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #creating a player object in the middle of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -19,7 +29,15 @@ def main():
                 return
         
         screen.fill((0, 0, 0))
+
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
+
+        #calling the .tick() method on the clock object dividing it by 1000 and saving the value in dt
+        dt = clock.tick(60) / 1000
+        
     
 
 
