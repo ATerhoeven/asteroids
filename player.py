4cc816f3a4be2c6a6bfdd5df8142253ab50f30dd
Andreas Terhoven <andreas.terhoven@live.de>
2026-07-18 02:15:37 +0200

    added cooldown to shots

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 import pygame
 
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x_pos: float, y_pos: float) -> None:
         super().__init__(x_pos, y_pos, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown_timer = 0
 
     def triangle(self) -> list[pygame.Vector2]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -24,6 +25,7 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 
     def update(self, dt: float) -> None:
+        self.shot_cooldown_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -35,6 +37,9 @@ class Player(CircleShape):
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_SPACE]:
+            if self.shot_cooldown_timer > 0:
+                return
+            self.shot_cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
             self.shoot()
 
     def move(self, dt: float) -> None:
